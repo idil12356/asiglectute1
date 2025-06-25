15ef2dc57845363a71741c2b06a8d8f7eaa12661
idil12356 <idlmhad@gmail.com>
2025-06-25 20:20:24 +0100

    dhalashada idil

--- asiglectute1/Program.cs
+++ asiglectute1/Program.cs
@@ -10,6 +10,25 @@ namespace asiglectute1
     {
         static void Main(string[] args)
         {
+            string name = "Idil Mahad Yusuf"; 
+            Console.WriteLine("Name: " + name);
+            string mothername = "Xamdi Ali";
+            Console.WriteLine("Momother'Name: " + mothername);
+            string dop = "2000-05-16";
+            Console.WriteLine("Date Of Birth: " + dop);
+            string pob = "Galkio";
+            Console.WriteLine("Place Of Pirt: " + pob);
+            string address = "Garsoor";
+            Console.WriteLine("Address: " + address);
+            int telphone = 097765567;
+            Console.WriteLine("Telphone: " + telphone);
+            string sex = "Female";
+            Console.WriteLine("Sex: " + sex);
+            string status = "Single";
+            Console.WriteLine("Status: " + status);
+
+
+
         }
     }
 }
