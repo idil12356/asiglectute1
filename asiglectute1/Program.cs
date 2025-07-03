35d9a426e85697ad7e00655fc3827ba1cf76394e
idil12356 <idlmhad@gmail.com>
2025-07-03 06:06:24 +0100

    Mini Project(Student Quiz System)

--- asiglectute1/Program.cs
+++ asiglectute1/Program.cs
@@ -10,29 +10,87 @@ namespace asiglectute1
     {
         static void Main(string[] args)
         {
-            string name = "Idil Mahad Yusuf";
-            Console.WriteLine("Name: " + name); 
-            string mothername = "Xamdi Ali";
-            Console.WriteLine("Momother'Name: " + mothername);
-           
-            int birthYear = 2000;
-            int birthMonth = 05;
-            int birthDay = 15;
-            DateTime dob = new DateTime(birthYear, birthMonth, birthDay);
-            Console.WriteLine("Date Of Birth: " + dob.ToString("yyyy-MM-dd"));
+            // Gali magaca iyo ID
+            Console.Write("Geli Magacaaga: ");
+            string name = Console.ReadLine();
 
-            string pob = "Galkio";
-            Console.WriteLine("Place Of Pirth: " + pob);
-            string address = "Garsoor";
-            Console.WriteLine("Address: " + address);
-            int telphone = 097765567;
-            Console.WriteLine("Telphone: " + telphone);
-            string sex = "Female";
-            Console.WriteLine("Sex: " + sex);
-            string status = "Single";
-            Console.WriteLine("Status: " + status);
+            Console.Write("Geli IDgaaga: ");
+            string id = Console.ReadLine();
 
+            Console.WriteLine("\n Dooro nooca xisaabta:");
+            Console.WriteLine("1. Kudar");
+            Console.WriteLine("2. Kajar");
+            Console.WriteLine("3. Kudhufo");
+            Console.WriteLine("4. Qaybi");
+            Console.Write("Dooro (1-4): ");
+            int choice = Convert.ToInt32(Console.ReadLine());
 
+            int correct = 0;
+            int total = 10;
+
+            Random rand = new Random();
+
+            for (int i = 1; i <= total; i++)
+            {
+                int a = rand.Next(1, 20);
+                int b = rand.Next(1, 20);
+                int answer = 0;
+                int userAnswer = 0;
+
+                switch (choice)
+                {
+                    case 1: // Kudar
+                        Console.Write($"Q{i}: {a} + {b} = ");
+                        answer = a + b;
+                        break;
+                    case 2: // Kajar
+                        Console.Write($"Q{i}: {a} - {b} = ");
+                        answer = a - b;
+                        break;
+                    case 3: // Kudhufo
+                        Console.Write($"Q{i}: {a} * {b} = ");
+                        answer = a * b;
+                        break;
+                    case 4: // Qaybi
+                        a = a * b; // si b ay u qaybin karto a
+                        Console.Write($"Q{i}: {a} / {b} = ");
+                        answer = a / b;
+                        break;
+                    default:
+                        Console.WriteLine("❌ Doorasho khaldan.");
+                        return;
+                }
+
+                try
+                {
+                    userAnswer = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine(" Kaliya tiro gelin.");
+                    continue;
+                }
+
+                if (userAnswer == answer)
+                {
+                    Console.WriteLine(" Sax!\n");
+                    correct++;
+                }
+                else
+                {
+                    Console.WriteLine($" Khalad. Jawaabta saxda ah waa: {answer}\n");
+                }
+            }
+
+            int wrong = total - correct;
+            int percent = (correct * 100) / total;
+
+            Console.WriteLine("===== Natiijada =====");
+            Console.WriteLine($" Magaca: {name}");
+            Console.WriteLine($" ID: {id}");
+            Console.WriteLine($" Sax: {correct}");
+            Console.WriteLine($" Khalad: {wrong}");
+            Console.WriteLine($" Boqolley: {percent}%");
 
 
         }
